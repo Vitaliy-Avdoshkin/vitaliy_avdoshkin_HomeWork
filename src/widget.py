958e04c5950c66c31c51372dad5acb8dee2b2fe4
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-06-17 16:21:39 +0300

    task2 second method

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-from datetime import datetime as dt
+from datetime import datetime
 
 from src.masks import get_mask_account, get_mask_card_number
 
@@ -26,10 +26,22 @@ print(mask_account_card("Visa Platinum 8990922113665229"))
 print(mask_account_card("Счет 73654108430135874305"))
 
 
-def get_data(datatime: str) -> str:
+# def get_data(datatime: str) -> str:
+#     """Функция преобразования даты"""
+#
+#     return f"{datatime[8:10]}.{datatime[5:7]}.{datatime[:4]} "
+#
+#
+# print(get_data("2018-07-11T02:26:18.671407"))
+
+
+"TASK 2 second method"
+
+def get_data(date: str) -> str:
     """Функция преобразования даты"""
 
-    return f"{datatime[8:10]}.{datatime[5:7]}.{datatime[:4]} "
+    date_update = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f')
+    return date_update.strftime("%d-%m-%Y")
 
 
 print(get_data("2018-07-11T02:26:18.671407"))
