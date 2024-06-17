e7ef67a5b2d498da7bdac9b69a585426ab66fc44
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-06-17 12:53:53 +0300

    update_functions

--- src/masks.py
+++ src/masks.py
@@ -3,10 +3,10 @@ def get_mask_card_number(card_number: int) -> str:
 
     card_number_str = str(card_number)
 
-    if len(card_number_str) == 16:
-        return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
-    else:
-        return "Неверный формат банковской карты"
+    #if len(card_number_str) == 16:
+    return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
+    #else:
+        #return "Неверный формат банковской карты"
 
 
 print(get_mask_card_number(7000792289606361))
@@ -17,10 +17,10 @@ def get_mask_account(accound_number: int) -> str:
 
     account_number_str = str(accound_number)
 
-    if len(account_number_str) == 20:
-        return f"**{account_number_str[-4:]}"
-    else:
-        return "Неверный формат номера счета"
+    #if len(account_number_str) == 20:
+    return f"**{account_number_str[-4:]}"
+    #else:
+        #return "Неверный формат номера счета"
 
 
 print(get_mask_account(73654108430135874305))
