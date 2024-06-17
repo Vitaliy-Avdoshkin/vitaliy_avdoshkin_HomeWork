840cf99c2ae833aacd8b916ddbfd5f0fa1866fd8
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-06-17 15:05:09 +0300

    task2 Done

--- src/widget.py
+++ src/widget.py
@@ -2,6 +2,7 @@ from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_info: str) -> str:
+    """Функция общей маскировки карты и счета"""
 
     account_number = account_info[account_info.rfind(" ") + 1 :]
 
@@ -21,3 +22,11 @@ def mask_account_card(account_info: str) -> str:
 
 print(mask_account_card("Visa Platinum 8990922113665229"))
 print(mask_account_card("Счет 73654108430135874305"))
+
+
+def get_data(datatime: str) -> str:
+
+    return f"{datatime[8:10]}.{datatime[5:7]}.{datatime[:4]} "
+
+
+print(get_data("2018-07-11T02:26:18.671407"))
