44899e303a818a3b25aa8f33a1292c1eb9b9921a
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-06-17 12:45:19 +0300

    original_functions

---
+++ src/masks.py
@@ -0,0 +1,26 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Функция маскировки номера карты"""
+
+    card_number_str = str(card_number)
+
+    if len(card_number_str) == 16:
+        return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
+    else:
+        return "Неверный формат банковской карты"
+
+
+print(get_mask_card_number(7000792289606361))
+
+
+def get_mask_account(accound_number: int) -> str:
+    """Функция маскировки номера счета"""
+
+    account_number_str = str(accound_number)
+
+    if len(account_number_str) == 20:
+        return f"**{account_number_str[-4:]}"
+    else:
+        return "Неверный формат номера счета"
+
+
+print(get_mask_account(73654108430135874305))
