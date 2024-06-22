dafb1bb015be31a3c48f2bdac4babfc7ed176f57
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-06-22 23:38:41 +0300

    Tasks are Done

---
+++ src/processing.py
@@ -0,0 +1,68 @@
+from typing import Iterable
+
+
+def filter_by_state(list: Iterable[list], state="EXECUTED") -> Iterable[list]:
+
+    filtered_list = []
+
+    for element in list:
+        if element["state"] == state:
+            filtered_list.append(element)
+
+    return filtered_list
+
+
+print(
+    filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {
+                "id": 939719570,
+                "state": "EXECUTED",
+                "date": "2018-06-30T02:08:58.425572",
+            },
+            {
+                "id": 594226727,
+                "state": "CANCELED",
+                "date": "2018-09-12T21:27:25.241689",
+            },
+            {
+                "id": 615064591,
+                "state": "CANCELED",
+                "date": "2018-10-14T08:21:33.419441",
+            },
+        ],
+        "CANCELED",
+    )
+)
+
+
+def sort_by_date(list: Iterable[list], ascending=True) -> Iterable[list]:
+
+    sorted_list = sorted(list, key=lambda x: x.get("date"), reverse=ascending)
+    return sorted_list
+
+
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {
+                "id": 939719570,
+                "state": "EXECUTED",
+                "date": "2018-06-30T02:08:58.425572",
+            },
+            {
+                "id": 594226727,
+                "state": "CANCELED",
+                "date": "2018-09-12T21:27:25.241689",
+            },
+            {
+                "id": 615064591,
+                "state": "CANCELED",
+                "date": "2018-10-14T08:21:33.419441",
+            },
+        ]
+
+    )
+)
