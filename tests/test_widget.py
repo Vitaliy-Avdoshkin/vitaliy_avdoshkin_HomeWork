49e3be27bf0825dc44d4ff919723d43544f3be6b
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-06-25 12:34:53 +0300

    HW 10.2 is DONE

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,24 +1,25 @@
-from src.widget import get_data
-from src.masks import get_mask_account, get_mask_card_number
 import pytest
 
+from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_data
 
-@pytest.mark.parametrize("x", ['Visa Platinum 8990922113665229', 'Счет 73654108430135874305', 'Maestro 1596837868705199', ''])
+
+@pytest.mark.parametrize(
+    "x",
+    [
+        "Visa Platinum 8990922113665229",
+        "Счет 73654108430135874305",
+        "Maestro 1596837868705199",
+        "",
+    ],
+)
 def test_mask_account_card(x):
-    account_number = x[x.rfind(' ') + 1:]
+    account_number = x[x.rfind(" ") + 1 :]
 
-    if 'Счет' in x:
-        return (
-                x[: x.rfind(' ')]
-                + ' '
-                + get_mask_account(account_number)
-        )
+    if "Счет" in x:
+        return x[: x.rfind(" ")] + " " + get_mask_account(account_number)
     else:
-        return (
-                x[: x.rfind(' ')]
-                + ' '
-                + get_mask_card_number(account_number)
-        )
+        return x[: x.rfind(" ")] + " " + get_mask_card_number(account_number)
 
 
 def test_get_data(input_date):
