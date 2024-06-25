c460c2dbbbfd2f854bee1197799c2de4ee1ae3df
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-06-25 19:53:21 +0300

    Edit code after mentor's comments

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,10 +1,11 @@
 from src.masks import get_mask_account, get_mask_card_number
 
+
 def test_get_mask_card_number():
-    assert get_mask_card_number('7000792289606361') == '7000 79** **** 6361'
-    assert get_mask_card_number('800079228960636') == 'Неверный формат банковской карты'
+    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
+    assert get_mask_card_number("800079228960636") == "Неверный формат банковской карты"
 
 
 def test_get_mask_account():
-    assert get_mask_account('73654108430135874305') == '**4305'
-    assert get_mask_account('786541084301358743') == 'Неверный формат номера счета'
+    assert get_mask_account("73654108430135874305") == "**4305"
+    assert get_mask_account("786541084301358743") == "Неверный формат номера счета"
