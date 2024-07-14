c6938943e31b7126db5886892e3cd63921b44b2a
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-07-14 21:39:45 +0300

    HomeWork_12_1 w/o tests

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,10 +1,10 @@
 from unittest.mock import patch
 
-from src.external_api import transactions_amount
+from src.external_api import transactions_amount, transaction
 
 
 @patch("requests.get")
 def test_transactions_amount(mock_get):
 
     mock_get.return_value.json.return_value = {"rates": {"RUB": 87.39642}}
-    assert transactions_amount(142264268) == 6914361.65
+    assert transactions_amount(transaction) == 718518.31
