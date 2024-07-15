b4c263d228a502ea0508770a5dac627f7f803f14
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-07-15 22:52:04 +0300

    HomeWork_12_2 is DONE

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,28 @@
+import logging
+
+# Добавляем логгер, который записывает логи в файл.
+logger = logging.getLogger("main")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(
+    r"D:\PYTHON\vitaliy_avdoshkin_HomeWork\logs\main.log", encoding="utf-8"
+)
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s: %(message)s"
+)
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера карты"""
 
     card_number_str = str(card_number)
 
     if len(card_number_str) == 16:
+        logger.info("Формат карты верный")
         return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
     else:
+        logger.warning("Неверный формат банковской карты")
         return "Неверный формат банковской карты"
 
 
@@ -18,8 +35,10 @@ def get_mask_account(accound_number: str) -> str:
     account_number_str = str(accound_number)
 
     if len(account_number_str) == 20:
+        logger.info("Формат счета верный")
         return f"**{account_number_str[-4:]}"
     else:
+        logger.warning("Неверный формат счета")
         return "Неверный формат номера счета"
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,18 @@
 import json
+import logging
 from typing import Any
+
+# Добавляем логгер, который записывает логи в файл.
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(
+    r"D:\PYTHON\vitaliy_avdoshkin_HomeWork\logs\utils.log", encoding="utf-8"
+)
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s: %(message)s"
+)
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_transactions_info(json_file: str) -> list[Any]:
@@ -7,9 +20,11 @@ def get_transactions_info(json_file: str) -> list[Any]:
 
     with open(json_file, "r", encoding="utf-8") as file:
         try:
+            logger.info("Путь до файла json верный")
             transactions_info = json.load(file)
             return transactions_info
         except:
+            logger.warning("Импортируемый список пуст или отсутствует.")
             return []
 
 
